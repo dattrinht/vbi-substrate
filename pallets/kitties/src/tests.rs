774bdc675d033c70a4e2846d305afd24052f8e50
Dat Trinh <dattrinh.net@gmail.com>
2022-03-20 14:39:13 +0700

    add tests for extrinsic calls

--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -50,3 +50,77 @@ fn create_kitty_should_work() {
 		assert_eq!(kitty.price, None);
 	});
 }
+
+#[test]
+fn transfer_kitty_should_work() {
+	new_test_ext(vec![
+		(1, *b"1234567890123456", Gender::Female),
+		(2, *b"123456789012345a", Gender::Male),
+	])
+	.execute_with(|| {
+		// check that account 10 own a kitty
+		assert_ok!(SubstrateKitties::create_kitty(Origin::signed(10)));
+		let id = KittiesOwned::<Test>::get(10)[0];
+
+		// account 10 send kitty to account 3
+		assert_ok!(SubstrateKitties::transfer(Origin::signed(10), 3, id));
+
+		// account 10 now has nothing
+		assert_eq!(KittiesOwned::<Test>::get(10).len(), 0);
+		// but account 3 does
+		assert_eq!(KittiesOwned::<Test>::get(3).len(), 1);
+		assert_ownership(3, id);
+	});
+}
+
+#[test]
+fn buy_kitty_should_work() {
+	new_test_ext(vec![
+		(1, *b"1234567890123456", Gender::Female),
+		(2, *b"123456789012345a", Gender::Male),
+	])
+	.execute_with(|| {
+		// Account #2 sets a price of 4 for their kitty
+		let id = KittiesOwned::<Test>::get(2)[0];
+		let set_price = 4;
+
+		assert_ok!(SubstrateKitties::set_price(Origin::signed(2), id, Some(set_price)));
+
+		// Account #1 can buy account #2's kitty
+		assert_ok!(SubstrateKitties::buy_kitty(Origin::signed(1), id, set_price));
+	});
+}
+
+#[test]
+fn set_price_should_work() {
+	new_test_ext(vec![
+		(1, *b"1234567890123456", Gender::Female),
+		(2, *b"123456789012345a", Gender::Male),
+	])
+	.execute_with(|| {
+		// New price is set to 4
+		let id = KittiesOwned::<Test>::get(2)[0];
+		let set_price = 4;
+		assert_ok!(SubstrateKitties::set_price(Origin::signed(2), id, Some(set_price)));
+	});
+}
+
+#[test]
+fn breed_kitty_should_work() {
+	new_test_ext(vec![
+		(1, *b"1234567890123456", Gender::Female),
+		(2, *b"123456789012345a", Gender::Male),
+	])
+	.execute_with(|| {
+		// Mint female kitty for account #1
+		let mom_dna = [0u8; 16];
+		let mom = SubstrateKitties::mint(&1, Some(mom_dna), Some(Gender::Female)).unwrap();
+
+		// Mint male kitty for account #1
+		let dad_dna = [1u8; 16];
+		let dad = SubstrateKitties::mint(&1, Some(dad_dna), Some(Gender::Male)).unwrap();
+
+		// Breeder can only breed kitties they own
+		assert_ok!(SubstrateKitties::breed_kitty(Origin::signed(1), mom, dad));
+	});
+}
